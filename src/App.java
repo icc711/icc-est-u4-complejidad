a30a817be0dbb64981c1376880a2ea42c4ad8f34
Your Name <you@example.com>
2026-04-14 18:37:52 -0500

    UPD: App file

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+         
+        int [] array = new int [] {1,2,3,4,5};
+        System.out.println(array[0];
+        System.out.println(array);
+        // FOR NORMAL
+        for(int i=0; i<array.lenght; i++) {
+            System.out.println(array[i]);
+    
     }
+        // FOR EACH
+        for(int i : array) {
+            System.out.println(i);
+            
+
 }
